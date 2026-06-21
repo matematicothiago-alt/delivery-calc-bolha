33b4cfe5ef1675bfe19c55490a2352f4bebb8cda
matematicothiago-alt <matematico.thiago@gmail.com>
2026-06-21 19:05:45 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name="LucroAoVivo"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LucroAoVivo"
 include(":app")

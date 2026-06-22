a684429f5591bbb55356b19d16648ccf1200b3b3
matematicothiago-alt <matematico.thiago@gmail.com>
2026-06-22 19:28:38 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.application") version "8.3.0"
-    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -15,10 +16,6 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures { 
-        viewBinding = true 
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,12 +23,16 @@ android {
         }
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     // ESSA PARTE RESOLVE O ERRO DO JVM 17:
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -40,4 +41,11 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    
+    // Room pra salvar as corridas no banco
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    
+    // Coroutines pro Room rodar em background
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
